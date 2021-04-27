4dab904d4d4fbf5a6768e4389b0b5fa07d6103c9
Vik <jzenterprize90@gmail.com>
2021-04-27 14:15:30 -0400

    Simple tuple

---
+++ SimpleTuple.py
@@ -0,0 +1,11 @@
+def the_tuple():
+    cords = (4, 5)
+    cords_two = [(4, 5), (6, 7), (80, 34)]  # A list of tuples
+    #  this_is_a_list = [4, 5]
+    #  Tuples are immutable and cannot be changed. Whereas a list can be changed
+    print(cords)
+    print(cords_two)
+    print(f"Index 0: {cords[0]}")
+    print(f"Index 0: {cords_two[0]}")
+
+the_tuple()
